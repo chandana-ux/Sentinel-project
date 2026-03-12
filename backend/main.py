632cddb78a42ba24e155627e735b8fba573ecacc
cc <xyx@gmail.com>
2026-03-13 04:34:20 +0530

    fix cors configuration

--- backend/main.py
+++ backend/main.py
@@ -49,12 +49,11 @@ app = FastAPI(title="Child Safety Layer API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://127.0.0.1:5174",
-        "http://127.0.0.1:5175",
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
+       allow_origins=[
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
+    "https://sentinelproj.netlify.app"
+]
     ],
     allow_credentials=True,
     allow_methods=["*"],
